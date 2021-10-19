7b4614ecbc40e052fc7fc0c072f692c0b74ba6b5
Cédric Wemmert <wemmert@unistra.fr>
2021-10-19 09:22:04 +0200

    update

--- evaluator.py
+++ evaluator.py
@@ -1,6 +1,5 @@
 import sys
 import json
-from termcolor import colored
 import numpy
 from math import *
 from functions import *
@@ -9,12 +8,12 @@ from optparse import OptionParser
 parser = OptionParser()
 parser.add_option("-f", "--file", dest="fileloc", help="Give your geojson file location")
 parser.add_option("-t", "--truth", dest="truthloc", help="Give your GroundTruth geojson file location")
-parser.add_option("-m", "--tmatch", dest="tmatch", default=50, help="Give your a maximal value for intersection matching. Default = 50m")
+parser.add_option("-m", "--tmatch", dest="tmatch", default=50, help="Give your maximal value for intersection matching. Default = 50m")
 
 (options, args) = parser.parse_args()
 
-if not options.fileloc: parser.error('File location not given')
-if not options.truthloc : parser.error('Truth file location not given')
+if not options.fileloc: parser.error('Missing result file')
+if not options.truthloc : parser.error('Missing ground truth file')
 
 #Parameters
 path = options.fileloc
@@ -56,26 +55,23 @@ for intersection in intersections :
     if distance * convCoef < Tmatch and ((intersectionTruth[0], intersectionTruth[1]) not in mapping or mapping[(intersectionTruth[0], intersectionTruth[1])][1] >= distance) :
         mapping[(intersectionTruth[0], intersectionTruth[1])] = (intersection, distance)
 
-#Compute the next respective four value : 
-# - Intersections that have been matched
-# - Intersections that have not been matched
-# - Intersections Truth that have been matched
-# - Intersections Truth that have not been matched
+# F-score calculation
+mappedTruthIntersections = [key for key in mapping.keys()]
+
 TP = [value[0] for value in mapping.values()]
 FP = [i for i in intersections if i not in TP]
-TN = [key for key in mapping.keys()]
-FN = [it for it in intersectionsTruth if (it[0], it[1]) not in TN]
+FN = [it for it in intersectionsTruth if (it[0], it[1]) not in mappedTruthIntersections]
 
-#Compute the 4 metrics
-precision = numpy.around(len(TN) / len(intersections) * 100, 2)
-recall = numpy.around(len(TN) / len(intersectionsTruth)  * 100, 2)
+precision = numpy.around(len(TP) / (len(TP)+len(FP)), 2)
+recall = numpy.around(len(TP) / (len(TP) + len(FN)), 2)
 fscore = numpy.around((2 * recall * precision) / (recall + precision), 2)
 accuracy = numpy.around(numpy.average([elem[1] for elem in mapping.values()]) * convCoef, 2)
-print(colored("###Intersection evaluation###", "magenta"))
-print(colored("Acceptation Distance = " + str(Tmatch) + "m", "green"))
-print("Precision = ", precision, "%")
-print("Recall = ", recall, "%")
-print("FScore = ", fscore, "%")
+
+print("### Intersections evaluation ###")
+print("Acceptation distance = " + str(Tmatch) + "m")
+print("Precision=", precision)
+print("Recall   =", recall)
+print("FScore   = ", fscore)
 print("Accuracy = ", accuracy, "m")
 print()
 
@@ -83,21 +79,18 @@ from geojson import Point, Feature, FeatureCollection, dump, LineString
 features = []
 
 for intersection in TP :
-    features.append(Feature(geometry=Point(intersection), properties={'marker-color': '#3fc700'}))
+    features.append(Feature(geometry=Point(intersection), properties={'marker-color': '#3fc700', 'title':'True postive'}))
 
 for intersection in FP :
-    features.append(Feature(geometry=Point(intersection), properties={'marker-color': '#d11f1f'}))
+    features.append(Feature(geometry=Point(intersection), properties={'marker-color': '#d11f1f', 'marker-symbol':'roadblock', 'title':'False positive'}))
 
-for intersection in intersectionsTruth :
-    features.append(Feature(geometry=Point(intersection), properties={'marker-color': '#e61edf'}))
+for intersection in FN :
+    features.append(Feature(geometry=Point(intersection), properties={'marker-color': '#545454', 'marker-symbol':'cross', 'title':'False negative'}))
 
-#e61edf
 feature_collection = FeatureCollection(features)
 
 with open('result.geojson', 'w') as f:
     dump(feature_collection, f)
-
-
 
 
 
@@ -129,17 +122,17 @@ FN = [segmentsTruth.index(segmentTruth) for segmentTruth in segmentsTruth if seg
 #avgt = averageOfNonDiagElem(floyd(adjacency_matrix(segmentsTruth)))
 
 #Compute the 6 metrics
-precision = numpy.around(len(TN) / len(segments) * 100, 2)
-recall = numpy.around(len(TN) / len(segmentsTruth) * 100, 2)
+precision = numpy.around(len(TN) / len(segments), 2)
+recall = numpy.around(len(TN) / len(segmentsTruth), 2)
 fscore = numpy.around((2 * recall * precision) / (recall + precision), 2)
 accuracy = numpy.around(numpy.average([distance for distance in mapping.values()]) * convCoef, 2)
 std = numpy.around(numpy.std([distance for distance in mapping.values()]) * convCoef, 2)
 #correctness = numpy.around(avgt / avgc, 2) * 100
 
-print(colored("###Segment evalution###", "magenta"))
-print("Precision = ", precision, "%")
-print("Recall = ", recall, "%")
-print("FScore = ", fscore, "%")
-print("Accuracy = ", accuracy, "m")
-print("Standard deviation = ", std, "m")
+print("### Segments evalution ###")
+print("Precision = ", precision)
+print("Recall    = ", recall)
+print("FScore    = ", fscore)
+print("Accuracy  = ", accuracy, "m")
+print("Std. dev. = ", std, "m")
 #print("Correctness = ", correctness, "%")
